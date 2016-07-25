ca16bce04fe0c6ead12d9ae543aa088aaa4fd4be
casco <alejandro.casco.fernandez@gmail.com>
2016-07-25 18:36:00 -0300

    Setup amber

---
+++ src/AmberEjemplo-Tests.js
@@ -0,0 +1,9 @@
+define("amber-amberejemplo/AmberEjemplo-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){"use strict";
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+$core.addPackage('AmberEjemplo-Tests');
+$core.packages["AmberEjemplo-Tests"].innerEval = function (expr) { return eval(expr); };
+$core.packages["AmberEjemplo-Tests"].transport = {"type":"amd","amdNamespace":"amber-amberejemplo"};
+
+$core.addClass('AmberEjemploTest', $globals.TestCase, [], 'AmberEjemplo-Tests');
+
+});
